4629d3267968ba2abaacbc5c731dc7898c4bf424
katy@karstaway.com <katy@karstaway.com>
2018-09-01 10:15:28 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,12 @@
+class Artist
+  
+  attr_accessor :name, :song
+  
+  def initialize
+  
+  @Song
+  
+end
+
+
+end 
---
+++ lib/author.rb
@@ -0,0 +1,13 @@
+class Author 
+  
+
+  attr_accessor :name, :artist
+  
+  def initialize
+  @Author
+  @Artist 
+  
+  
+end
+
+end
